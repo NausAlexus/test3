cdc5c196666fbf2423ad86852af69c2cdca7c9fe
DenisBardashevich <ackapbinka@mail.ru>
2025-03-12 00:08:21 +0300

    FIX

--- thanks_form.php
+++ thanks_form.php
@@ -3,12 +3,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $message = $_POST["message"];
     
     if (isset($_POST["privacypolicy"])) {
         $to = "support@" . $_SERVER['HTTP_HOST'];
         $subject = "New Contact Form Submission";
         $body = "Name: $name\n";
         $body .= "Email: $email\n";
+        $body .= "Message: $message\n";
         $body .= "Message: The user has agreed to the Privacy Policy.\n";
         
         $headers = "From: $to \r\n";
